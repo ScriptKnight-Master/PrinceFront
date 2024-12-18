2f1ff958d262b482bf880f3423bf84677e5a4589
ScriptKnight.Master <stealthleopard0627@gmail.com>
2024-12-17 19:02:41 -0500

    setup screening config for TailwindCSS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,30 @@ export default {
   ],
   theme: {
     extend: {
+      container: {
+        center: true,
+        padding: "2rem",
+        screens: {
+          sm: "480px",
+          md: "640px",
+          lg: "768px",
+          xl: "1024px",
+          "2xl": "1400px",
+          xxl: "1536px",
+        },
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      xxl: "1920px",
     },
   },
   plugins: [],
